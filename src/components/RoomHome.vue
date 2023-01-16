ad765d6551958b0a8b4ab41ab648f1b0acd83a12
mateocles <mat.bermudez@udla.edu.co>
2023-01-16 01:08:10 -0500

    test take photo

--- src/components/RoomHome.vue
+++ src/components/RoomHome.vue
@@ -113,7 +113,7 @@ export default {
 			const context = this.$refs.canvas.getContext('2d');
 			console.log(window.innerWidth);
 			console.log(window.innerHeight);
-			if (window.innerHeight === 1180) {
+			if (window.innerHeight === 1061) {
 				context.drawImage(this.$refs.camera, 0, 0, 320, 550);
 			} else {
 				context.drawImage(this.$refs.camera, 0, 0, 320, 240);
